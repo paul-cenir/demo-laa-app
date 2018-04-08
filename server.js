d1d8bed4f8fd4c77943416b47ef4c3bc7e59bc91
paul.cenir/home <paul.cenir03@gmail.com>
2018-04-08 09:47:18 +0800

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+
+
+app.set('port', (process.env.PORT || 5000));
+
+// Start node server
+app.listen(app.get('port'), function () {
+    console.log('Node server is running on port ' + app.get('port'));
+});
