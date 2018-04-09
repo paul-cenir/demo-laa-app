a0fb02d3b9be50b3d0d49d366acda02733f0889a
paul.cenir/home <paul.cenir03@gmail.com>
2018-04-09 19:03:39 +0800

    for updates

--- src/app/modules/global-pages/global-pages.module.ts
+++ src/app/modules/global-pages/global-pages.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: [FooterComponent]
+    declarations: []
 })
 export class GlobalPagesModule { }
