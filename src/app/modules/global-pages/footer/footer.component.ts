eabfd653531e493458404d0c54164b0132b08efa
paul.cenir/home <paul.cenir03@gmail.com>
2018-04-14 16:22:39 +0800

    for updates

--- src/app/modules/global-pages/footer/footer.component.ts
+++ src/app/modules/global-pages/footer/footer.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+    showBox: boolean = false;
+    constructor() { }
 
-  constructor() { }
+    clickShowHideBox() {
+        if (this.showBox) {
+            this.showBox = false;
+        } else {
+            this.showBox = true;
+        }
 
-  ngOnInit() {
-  }
+
+        window.scrollTo(0, document.body.scrollHeight);
+        setTimeout(function () {
+            $('html,body').animate({ scrollTop: document.body.scrollHeight });
+        }, 0);
+    }
+
+
+    ngOnInit() {
+
+
+    }
 
 }
