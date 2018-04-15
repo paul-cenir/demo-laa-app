6772177301d7e11ef2dfce073beff2a70e832c07
paul.cenir/home <paul.cenir03@gmail.com>
2018-04-15 21:06:37 +0800

    updates in heroku

--- src/app/modules/pages/about/about.component.ts
+++ src/app/modules/pages/about/about.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+    activeStepsData: number = 2018;
+    constructor() { }
+    activeSteps(active) {
+        this.activeStepsData = active;
+    }
+    ngOnInit() {
+    }
 
 }
--- src/app/modules/pages/home-page/home-page.component.ts
+++ src/app/modules/pages/home-page/home-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../../app.component';
 import { AppGlobalApiService } from '../../../app-global-api/app-global-api.service'
-
+declare var $: any;
 @Component({
     selector: 'app-home-page',
     templateUrl: './home-page.component.html',
@@ -20,6 +20,8 @@ export class HomePageComponent implements OnInit {
         this.name = this.globalDataService.globalData.name;
         this.title = this.globalDataService.globalData.title;
         this.lisOfHeroes = this.globalDataService.lisOfHeroes;
+
+
     }
 
 }
